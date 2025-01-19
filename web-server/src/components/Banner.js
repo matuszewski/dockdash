86857d11da66d70d8dbb1a83e85d8c4e815744ed
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2025-01-19 20:59:36 +0100

    changed colors in gui

--- web-server/src/components/Banner.js
+++ web-server/src/components/Banner.js
@@ -3,12 +3,11 @@ import React from 'react'
 function Banner() {
    return (
       <section className='Banner'>
-         <div className="row m-3">
-            <div className='col p-5 bg-success rounded-3'>
-               <h1>Banner Component</h1>
+         <div className="row m-3 mt-5">
+            <div className='col my-5 rounded-3'>
 
                <div class="row">
-                  <div class="col-md-6 p-5 bg-light rounded">
+                  <div class="col-md-10 p-5 bg-secondary rounded-3">
                      <h1><b>: :</b> dock<b>dash</b></h1>
                      <p>Część projektowa pracy magisterskiej</p>
                      <table class="table">
@@ -32,8 +31,41 @@ function Banner() {
                         </tbody>
                      </table>
                   </div>
-
                </div>
+
+            </div>
+
+
+
+
+            <div className='col my-5 rounded-3'>
+               <div class="row">
+                  <div class="col-md-6 p-5 bg-secondary rounded-3">
+                     <h1>Zasoby</h1>
+                     <p>Część projektowa pracy magisterskiej</p>
+                     <table class="table">
+                        <tbody>
+                           <tr>
+                              <td>autor</td>
+                              <td>Krzysztof Matuszewski</td>
+                           </tr>
+                           <tr>
+                              <td>indeks</td>
+                              <td>160802</td>
+                           </tr>
+                           <tr>
+                              <td>email</td>
+                              <td>krzysiekmatuszewski@outlook.com</td>
+                           </tr>
+                           <tr>
+                              <td>github</td>
+                              <td>@matuszewski</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+               </div>
+
             </div>
          </div>
       </section>
--- web-server/src/components/Instances.js
+++ web-server/src/components/Instances.js
@@ -4,7 +4,7 @@ function Instances() {
    return (
       <section className='Instances'>
          <div className="row m-3">
-            <div className='col p-5 bg-success rounded-3'>
+            <div className='col p-5 bg-secondary    rounded-3'>
                <h1>Instances Component</h1>
             </div>
          </div>
