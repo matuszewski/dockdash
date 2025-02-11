58f5647ae4c9e8c74ba4c9c596a1202910587f23
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2025-02-11 20:49:46 +0100

    added author page redesigned

--- web-server/src/pages/Author.js
+++ web-server/src/pages/Author.js
@@ -1,43 +1,89 @@
-import React from 'react'
+import React from "react";
 
-// import mui icons
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import navigation and footer components
+import Navigation from "../components/Navigation.js";
+import Footer from "../components/Footer.js";
+
+// import icons
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 
 function Author() {
-  return (
-    <div className='Author'>
-      <div class="row m-0 m-sm-5">
-        <div class="col-12 col-xl-12">
-          <div class="card card-body border-0 shadow mb-4 text-start p-5">
-            <h4 class="h4 mb-4"><strong>Autor</strong></h4>
-            <p class="h1 fw-bold">Krzysztof <text class="text-muted">Matuszewski</text></p>
+   return (
+      <div className="Author">
+         <Navigation />
+         <div className="row m-5 p-5" /> {/* empty row just for margin */}
+         <div className="row m-5">
+            <div className="col-lg-12 col-md-7 col-12">
+               <div className="row">
+                
+                <div className="col-3 p-5 bg-light rounded-3">
 
-            <div class="mt-1 mb-4">
-              <button type="button" class="btn btn-dark me-2"><MailOutlineIcon/></button>
-              <button type="button" class="btn btn-dark me-2"><GitHubIcon/></button>
+               <h1 className="d-flex align-items-center">
+                     <AccountBoxIcon />
+                     &nbsp;Autor
+                  </h1>
+
+                  <p class="h1 fw-bold">
+                     Krzysztof <text class="text-muted">Matuszewski</text>
+                  </p>
+
+                  <div class="mt-1 mb-4">
+                     <button type="button" class="btn btn-dark me-2">
+                        <MailOutlineIcon />&nbsp;&nbsp; Email
+                     </button>
+                     <button type="button" class="btn btn-dark me-2">
+                        <GitHubIcon /> &nbsp;&nbsp; GitHub {/* TODO: add link here and above */}
+                     </button>
+                  </div>
+
+                  <p class="text-dark mb-2">AHE Łódź 2024/2025</p>
+                  <p class="text-dark mb-2">Część projektowa pracy magisterskiej.</p>
+
+
+</div>
+
+
+
+
+                  <div className="col-3 p-5 bg-light rounded-3">
+                     <table class="table table-light table-hover">
+                        <tbody>
+                           <tr>
+                              <th className="p-3" scope="row">stopień</th>
+                              <td className="p-3">II</td>
+                           </tr>
+                           <tr>
+                              <th className="p-3" scope="row">kierunek</th>
+                              <td className="p-3">informatyka</td>
+                           </tr>
+                           <tr>
+                              <th className="p-3" scope="row">specializacja</th>
+                              <td className="p-3">sieci teleinformatyczne</td>
+                           </tr>
+                           <tr>
+                              <th className="p-3" scope="row">rok</th>
+                              <td className="p-3">II</td>
+                           </tr>
+                           <tr>
+                              <th className="p-3" scope="row">semestr</th>
+                              <td className="p-3">3</td>
+                           </tr>
+                           <tr>
+                              <th className="p-3" scope="row">numer indeksu</th>
+                              <td className="p-3">160802</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+
+               </div>
             </div>
-
-            <p class="text-muted mb-2">AHE Łódź 2025</p>
-            <ol class="list-unstyled mb-0">
-              <li class="m-0">
-                <p class="m-0">kierunek: <text class="text-muted">informatyka</text></p>
-              </li>
-              <li class="m-0">
-                <p class="m-0">specializacja: <text class="text-muted">sieci teleinformatyczne</text></p>
-              </li>
-              <li class="m-0">
-                <p class="m-0">semestr: <text class="text-muted">3</text></p>
-              </li>
-              <li class="m-0">
-                <p class="m-0">rok: <text class="text-muted">2</text></p>
-              </li>
-            </ol>
-          </div>
-        </div>
+         </div>
+         <Footer />
       </div>
-    </div>
-  );
+   );
 }
 
 export default Author;
