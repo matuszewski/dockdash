4ab1cf277ba964930e6db7ab5af70d35e719136f
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2025-02-14 09:10:48 +0100

    formatted

--- web-server/src/pages/Author.js
+++ web-server/src/pages/Author.js
@@ -17,67 +17,75 @@ function Author() {
          <div className="row m-5">
             <div className="col-lg-12 col-md-7 col-12">
                <div className="row">
-                
-                <div className="col-3 p-5 bg-light rounded-3">
+                  <div className="col-3 p-5 bg-light rounded-3">
+                     <h1 className="d-flex align-items-center">
+                        <AccountBoxIcon />
+                        &nbsp;Autor
+                     </h1>
 
-               <h1 className="d-flex align-items-center">
-                     <AccountBoxIcon />
-                     &nbsp;Autor
-                  </h1>
+                     <p class="h1 fw-bold">
+                        Krzysztof <text class="text-muted">Matuszewski</text>
+                     </p>
 
-                  <p class="h1 fw-bold">
-                     Krzysztof <text class="text-muted">Matuszewski</text>
-                  </p>
+                     <div class="mt-1 mb-4">
+                        <button type="button" class="btn btn-dark me-2">
+                           <MailOutlineIcon />
+                           &nbsp;&nbsp; Email
+                        </button>
+                        <button type="button" class="btn btn-dark me-2">
+                           <GitHubIcon /> &nbsp;&nbsp; GitHub{" "}
+                           {/* TODO: add link here and above */}
+                        </button>
+                     </div>
 
-                  <div class="mt-1 mb-4">
-                     <button type="button" class="btn btn-dark me-2">
-                        <MailOutlineIcon />&nbsp;&nbsp; Email
-                     </button>
-                     <button type="button" class="btn btn-dark me-2">
-                        <GitHubIcon /> &nbsp;&nbsp; GitHub {/* TODO: add link here and above */}
-                     </button>
+                     <p class="text-dark mb-2">AHE Łódź 2024/2025</p>
+                     <p class="text-dark mb-2">
+                        Część projektowa pracy magisterskiej.
+                     </p>
                   </div>
-
-                  <p class="text-dark mb-2">AHE Łódź 2024/2025</p>
-                  <p class="text-dark mb-2">Część projektowa pracy magisterskiej.</p>
-
-
-</div>
-
-
-
 
                   <div className="col-3 p-5 bg-light rounded-3">
                      <table class="table table-light table-hover">
                         <tbody>
                            <tr>
-                              <th className="p-3" scope="row">stopień</th>
+                              <th className="p-3" scope="row">
+                                 stopień
+                              </th>
                               <td className="p-3">II</td>
                            </tr>
                            <tr>
-                              <th className="p-3" scope="row">kierunek</th>
+                              <th className="p-3" scope="row">
+                                 kierunek
+                              </th>
                               <td className="p-3">informatyka</td>
                            </tr>
                            <tr>
-                              <th className="p-3" scope="row">specializacja</th>
+                              <th className="p-3" scope="row">
+                                 specializacja
+                              </th>
                               <td className="p-3">sieci teleinformatyczne</td>
                            </tr>
                            <tr>
-                              <th className="p-3" scope="row">rok</th>
+                              <th className="p-3" scope="row">
+                                 rok
+                              </th>
                               <td className="p-3">II</td>
                            </tr>
                            <tr>
-                              <th className="p-3" scope="row">semestr</th>
+                              <th className="p-3" scope="row">
+                                 semestr
+                              </th>
                               <td className="p-3">3</td>
                            </tr>
                            <tr>
-                              <th className="p-3" scope="row">numer indeksu</th>
+                              <th className="p-3" scope="row">
+                                 numer indeksu
+                              </th>
                               <td className="p-3">160802</td>
                            </tr>
                         </tbody>
                      </table>
                   </div>
-
                </div>
             </div>
          </div>
