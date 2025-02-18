c5c4c0bc88c6d666db57ef3d660fa6397b15ef84
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2025-02-18 18:57:08 +0100

    added latest changes to api-server

--- api-server/server.js
+++ api-server/server.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
-
 import axios from "axios"; // for handling HTTP requests
 import colors from "colors"; // for colored logs
-import fs from "fs" // for loading and saving to files
+import fs from "fs"; // for loading and saving to files
 
 import logging from "./logging.js";
 const { debug, success, failure, info, banner } = logging;
@@ -18,12 +17,16 @@ app.use(cors());
 const api_server_port = 4000;
 
 // initialize instances var
-let instances = {}
+let instances = {};
 
-
-
+/**
+ * Loads instances configuration file
+ * @param {string} [config_file_path="./config/instances.json"] Path to instances config file
+ * @returns {Object} JSON object of instances configuration
+ * @throws {Error} Throws error when file is not found or file could not be opened
+ */
 function loadInstancesConfig(config_file_path = "./config/instances.json") {
-   let instances_config = {}
+   let instances_config = {};
    try {
       if (fs.existsSync(config_file_path)) {
          const data = fs.readFileSync(config_file_path, "utf8");
@@ -35,43 +38,64 @@ function loadInstancesConfig(config_file_path = "./config/instances.json") {
    } catch (error) {
       console.error("Error loading config file:", error);
    }
-   return instances_config
+   return instances_config;
 }
 
-
-
-
-function saveInstancesConfig(instances, config_file_path = "./config/instances.json") {
+/**
+ * Saves instances configuration to file
+ * @param {*} instances Instance configuration JSON object to save to JSON file
+ * @param {string} [config_file_path="./config/instances.json"] Path to JSON file to save the instances config
+ * @returns {void} Returns nothing
+ * @throws {Error} Throws error when file is not found or file could not be opened
+ */
+function saveInstancesConfig(
+   instances,
+   config_file_path = "./config/instances.json"
+) {
    try {
-       const data = JSON.stringify(instances, null, 4);
-       fs.writeFileSync(config_file_path, data, "utf8");
-       console.log(`${success()} instances config successfully saved to ${config_file_path.green}`);
+      const data = JSON.stringify(instances, null, 4);
+      fs.writeFileSync(config_file_path, data, "utf8");
+      console.log(
+         `${success()} instances config successfully saved to ${
+            config_file_path.green
+         }`
+      );
    } catch (error) {
-       console.error(`${failure()} error occured while saving instances file: ${error.red}`);
+      console.error(
+         `${failure()} error occured while saving instances file: ${error.red}`
+      );
    }
 }
-
-
 
 // TODO: get from config and make global
 const VERBOSE_MODE = true;
 const RAW_MODE = false;
-const INSTANCE_COMEUP_TIMEOUT = 5000; // 5 seconds
+const INSTANCE_RESPONSE_TIMEOUT = 5000; // 5 seconds
+const CONTAINER_RESPONSE_TIMEOUT = 5000; // 5 seconds
+const IMAGE_RESPONSE_TIMEOUT = 5000; // 5 seconds
+const RESOURCE_RESPONSE_TIMEOUT = 5000; // 5 seconds
 
-//const config = ?
 function loadServerConfig(config_file_path = "./config/settings.json") {
    // TODO: add loading server config from separate json file
-
    return 0;
 }
 
+/**
+ * Converts size from bytes to megabytes
+ * @param {number} bytes Size in bytes
+ * @returns {number} Size in megabytes
+ */
 function formatBytesToMB(bytes) {
-   const sizeInMB = bytes / (1024 * 1024); // converting
+   const sizeInMB = bytes / (1024 * 1024);
    return sizeInMB.toFixed(2);
 }
 
+/**
+ * Checks Docker instance availablity
+ * @param {string} instance_name Name of the Docker instance used in instances config
+ * @returns {boolean} True/false value if the given instance is available
+ */
 async function checkInstanceAvailablity(instance_name) {
-
    const { ip, port, api_version } = instances[instance_name];
    const url = `http://${ip}:${port}/v${api_version}/info`; // TODO: check default docker url for checking if the api works / status
    console.info(`${info()} composed URL: ${url.blue}`);
@@ -82,12 +106,15 @@ async function checkInstanceAvailablity(instance_name) {
    // );
 
    try {
-      // TODO: fix that
-      const response =  await axios.get(url, { timeout: INSTANCE_COMEUP_TIMEOUT }); // 5 seconds timeout // TODO: pick timeout from config/settings.json
+      const response = await axios.get(url, {
+         timeout: INSTANCE_RESPONSE_TIMEOUT,
+      });
       console.debug(`${debug()} instance ${instance_name.blue} is available`);
       return true;
    } catch (error) {
-      console.debug(`${debug()} instance ${instance_name.blue} is not available`);
+      console.debug(
+         `${debug()} instance ${instance_name.blue} is not available`
+      );
       return false;
    }
 }
@@ -95,20 +122,22 @@ async function checkInstanceAvailablity(instance_name) {
 function updateInstancesAvailabilty(inst) {
    /* Function for checking many Docker instances availabilty based on provided instances ID's */
 
-   let return_instances = inst
+   let return_instances = inst;
 
    try {
       Object.entries(inst).forEach(async ([key, instance]) => {
-         let instance_status = await checkInstanceAvailablity(key)
+         let instance_status = await checkInstanceAvailablity(key);
 
          //console.debug(`${debug()} instance: ${key},\t ip: ${instance.ip},\t port: ${instance.port},\t status: ${instance_status}`);
-         return_instances[key].status = instance_status
+         return_instances[key].status = instance_status;
       });
 
       return return_instances;
    } catch (error) {
       console.error(
-         `${failure()} a problem occured in updateInstancesAvailabilty(instances) function. error: ${error.red}`
+         `${failure()} a problem occured in updateInstancesAvailabilty(instances) function. error: ${
+            error.red
+         }`
       );
       return 1;
    }
@@ -130,7 +159,7 @@ app.get("/api/instances", async (req, res) => {
 
    // save
    // TODO
-   saveInstancesConfig(updated_instances)
+   saveInstancesConfig(updated_instances);
 
    return res.send(loadInstancesConfig());
 });
@@ -153,12 +182,14 @@ app.get("/api/:instance/containers", async (req, res) => {
    }
 
    const { ip, port, api_version } = instanceConfig;
-   const url = `http://${ip}:${port}/v${api_version}/containers/json?all=true`; // TODO: remove todo if its correct
+   const url = `http://${ip}:${port}/v${api_version}/containers/json?all=true`;
 
    try {
       // call docker instance
       console.info(`${info()} fetching containers from ${url.cyan}`);
-      const response = await axios.get(url, { timeout: 5000 }); // 5 seconds timeout
+      const response = await axios.get(url, {
+         timeout: CONTAINER_RESPONSE_TIMEOUT,
+      });
       const containers = response.data;
       console.info(`${success()} Docker API responded successfully`);
 
@@ -174,16 +205,18 @@ app.get("/api/:instance/containers", async (req, res) => {
          const return_structure = [];
 
          containers.forEach((container) => {
-
+            // get container's id
             const container_id = container.Id;
 
+            // get container's name
             const container_name = container.Names;
 
+            // get container's status
             const container_status = container.State
                ? container.State
                : "Unknown"; // Status of the container (e.g., running, exited)
 
-            // get container's creation date in a friendly Polish format (DD.MM.YYYY, 24-hour format)
+            // get container's creation time in a friendly format (DD.MM.YYYY, 24-hour format)
             const createdDate = container.Created
                ? new Date(container.Created * 1000).toLocaleString("pl-PL", {
                     day: "2-digit",
@@ -196,18 +229,13 @@ app.get("/api/:instance/containers", async (req, res) => {
                  })
                : "Unknown date";
 
-            // extract exposed ports (if any)
+            // get container's exposed ports (if any)
             const exposedPorts = container.Ports
                ? container.Ports.map((port) => port.PublicPort).join(", ")
                : "No exposed ports";
 
-            // get the image used to create the container
+            // get container's image
             const container_image = container.Image || "No image";
-
-            // get the size of the container (if available)
-            const container_size = container.SizeRootFs
-               ? formatBytesToMB(container.SizeRootFs)
-               : "Unknown size";
 
             // prepare the parsed data
             const container_data = {
@@ -217,21 +245,11 @@ app.get("/api/:instance/containers", async (req, res) => {
                created: createdDate,
                ports: exposedPorts,
                image: container_image,
-               size: container_size,
             };
 
             // append the parsed data to the return structure
             return_structure.push(container_data);
          });
-
-         // print return structure
-         // console.debug(
-         //    `${debug()} prepared return structure:\n${JSON.stringify(
-         //       return_structure,
-         //       null,
-         //       2
-         //    )}`
-         // );
 
          return res.json(return_structure);
       }
@@ -239,8 +257,9 @@ app.get("/api/:instance/containers", async (req, res) => {
       if (error.response) {
          // server responded with a status code other than 2xx
          console.error(
-            `${failure()} Docker API returned error: ${error.response.status} ${error.response.statusText}`
-               .red
+            `${failure()} Docker API returned error: ${error.response.status} ${
+               error.response.statusText
+            }`.red
          );
          return res
             .status(error.response.status)
@@ -284,7 +303,9 @@ app.get("/api/:instance/images", async (req, res) => {
    try {
       // call docker instance
       console.info(`${info()} fetching images from ${url.cyan}`);
-      const response = await axios.get(url, { timeout: 5000 }); // 5 seconds timeout
+      const response = await axios.get(url, {
+         timeout: IMAGE_RESPONSE_TIMEOUT,
+      });
       const images = response.data;
       console.info(`${success()} Docker API responded successfully`);
 
@@ -381,8 +402,9 @@ app.get("/api/:instance/images", async (req, res) => {
       if (error.response) {
          // server responded with a status code other than 2xx
          console.error(
-            `${failure()} Docker API returned error: ${error.response.status} ${error.response.statusText}`
-               .red
+            `${failure()} Docker API returned error: ${error.response.status} ${
+               error.response.statusText
+            }`.red
          );
          return res
             .status(error.response.status)
@@ -438,7 +460,9 @@ app.get("/api/:instance/resources", async (req, res) => {
 
    try {
       console.info(`${info()} fetching container resources from ${url.cyan}`);
-      const response = await axios.get(url, { timeout: 5000 });
+      const response = await axios.get(url, {
+         timeout: CONTAINER_RESPONSE_TIMEOUT,
+      });
       const containers = response.data;
       console.info(`${success()} Docker API responded successfully`);
 
@@ -447,7 +471,9 @@ app.get("/api/:instance/resources", async (req, res) => {
       for (const container of containers) {
          const statsUrl = `http://${ip}:${port}/v${api_version}/containers/${container.Id}/stats?stream=false`;
          try {
-            const statsResponse = await axios.get(statsUrl, { timeout: 5000 });
+            const statsResponse = await axios.get(statsUrl, {
+               timeout: RESOURCE_RESPONSE_TIMEOUT,
+            });
             const stats = statsResponse.data;
 
             const container_data = {
@@ -486,8 +512,9 @@ app.get("/api/:instance/resources", async (req, res) => {
             return_structure.push(container_data);
          } catch (statsError) {
             console.error(
-               `${failure()} Failed to fetch stats for container ${container.Id}: ${statsError.message}`
-                  .red
+               `${failure()} Failed to fetch stats for container ${
+                  container.Id
+               }: ${statsError.message}`.red
             );
          }
       }
@@ -505,8 +532,9 @@ app.get("/api/:instance/resources", async (req, res) => {
    } catch (error) {
       if (error.response) {
          console.error(
-            `${failure()} Docker API returned error: ${error.response.status} ${error.response.statusText}`
-               .red
+            `${failure()} Docker API returned error: ${error.response.status} ${
+               error.response.statusText
+            }`.red
          );
          return res
             .status(error.response.status)
@@ -526,31 +554,39 @@ app.get("/api/:instance/resources", async (req, res) => {
 // start the api server
 app.listen(api_server_port, () => {
    // print banner
-   console.info(banner)
-   console.info('====================== api-server 1.0 ==\nauthor: ','Krzysztof Matuszewski'.blue,', nr. ','160802'.blue, '\n')
-   console.info('|______timestamp_______|__module__|___label___|______________________log_message_____________________|'.gray,'\n')
+   console.info(banner);
+   console.info(
+      "====================== api-server 1.0 ==\nauthor: ",
+      "Krzysztof Matuszewski".blue,
+      ", nr. ",
+      "160802".blue,
+      "\n"
+   );
+   console.info(
+      "|______timestamp_______|__module__|___label___|______________________log_message_____________________|"
+         .gray,
+      "\n"
+   );
 
-   console.info(`${success()} DockDash API server is running on port ${String(api_server_port).blue}`);
-
+   console.info(
+      `${success()} DockDash API server is running on port ${
+         String(api_server_port).blue
+      }`
+   );
 
    // print info
-   console.info(`${info()} loading api-server config...`)
-
+   console.info(`${info()} loading api-server config...`);
 
    // print info
-   console.info(`${info()} loading instances config...`)
+   console.info(`${info()} loading instances config...`);
    // load instances
-   instances = loadInstancesConfig()
+   instances = loadInstancesConfig();
    // first time check instances availabilty (and save to instances_configuration their statuses)
-   console.info(`${info()} validating instances availabilty...`)
+   console.info(`${info()} validating instances availabilty...`);
 
-   let checked_instances = updateInstancesAvailabilty(instances)
+   let checked_instances = updateInstancesAvailabilty(instances);
 
-   
    // save instances
-   console.info(`${info()} saving instances to instance config...`)
-   saveInstancesConfig(checked_instances)
-
-
-
+   console.info(`${info()} saving instances to instance config...`);
+   saveInstancesConfig(checked_instances);
 });
