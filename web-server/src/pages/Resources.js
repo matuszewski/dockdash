f0a7f543e0992c5c3fb6a92467459884c2893d2d
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2025-03-09 18:09:59 +0100

    fixed resources panel

--- web-server/src/pages/Resources.js
+++ web-server/src/pages/Resources.js
@@ -6,12 +6,58 @@ import Footer from "../components/Footer.js";
 import LoadingAlert from "../components/LoadingAlert.js";
 
 // import components from recharts library
-import { XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart, PieChart, Pie, Cell, Area, ResponsiveContainer } from "recharts";
-
+import { XAxis, YAxis, CartesianGrid, BarChart, Bar, Tooltip, Legend, AreaChart, PieChart, Pie, Cell, Area, ResponsiveContainer } from "recharts";
 // import config file
 import config from "../config.json";
 
 const COLORS = ["#47A025", "#00C49F", "darkcyan", "slateblue", "#F2DD6E", "#FE4A49", "#2AB7CA"];
+
+
+const data = [
+   {
+     name: 'Page A',
+     uv: 4000,
+     pv: 2400,
+     amt: 2400,
+   },
+   {
+     name: 'Page B',
+     uv: 3000,
+     pv: 1398,
+     amt: 2210,
+   },
+   {
+     name: 'Page C',
+     uv: 2000,
+     pv: 9800,
+     amt: 2290,
+   },
+   {
+     name: 'Page D',
+     uv: 2780,
+     pv: 3908,
+     amt: 2000,
+   },
+   {
+     name: 'Page E',
+     uv: 1890,
+     pv: 4800,
+     amt: 2181,
+   },
+   {
+     name: 'Page F',
+     uv: 2390,
+     pv: 3800,
+     amt: 2500,
+   },
+   {
+     name: 'Page G',
+     uv: 3490,
+     pv: 4300,
+     amt: 2100,
+   },
+ ];
+
 
 function ResourcePieChart({ resources, resource }) {
    // prepare chart data
@@ -124,6 +170,31 @@ function RamPieChart({ resources }) {
    );
 }
 
+function CPUBarChart({ resources }) {
+   // mapping data to correct format
+   const data = resources.map((container) => ({
+      name: container.name,
+      cpu_usage: isNaN(parseFloat(container.cpu_usage)) ? 0 : parseFloat(container.cpu_usage), // safety check
+   }));
+
+   return (
+      <ResponsiveContainer width="100%" height={350}>
+         <BarChart data={data} margin={{ top: 10, right: 5, left: 5, bottom: 70 }}>
+            <CartesianGrid strokeDasharray="0.2 0.2" />   
+            <XAxis dataKey="name" tick={{ fontSize: 7 }} interval={0} textAnchor="end" angle={-90} dy={10} />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="cpu_usage" fill={COLORS[3 % COLORS.length]} />
+         </BarChart>
+      </ResponsiveContainer>
+   );
+}
+
+ 
+
+
+
+
 function Resources() {
    const [loaded, setLoaded] = useState(false);
    const [resources, setResources] = useState([]);
@@ -158,7 +229,8 @@ function Resources() {
             <div className="col-lg-4 col-md-3 col-12">
                <div className="p-4 rounded-3 bg-light text-dark">
                   <h4>CPU</h4>
-                  {!loaded ? <LoadingAlert /> : <ResourcePieChart resources={resources} resource={"cpu"} />}
+                  {!loaded ? <LoadingAlert /> :  <CPUBarChart resources={resources} />}
+                  {/* <ResourcePieChart resources={resources} resource={"cpu"} /> */}
                </div>
             </div>
 
